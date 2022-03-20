9a0b3b1064a9ca67efe3b7f9a5f1532ca20b7568
beisbolke322 <mykolasp999@gmail.com>
2022-03-20 11:05:38 +0200

    Dedu daugiau kodo

---
+++ registracija/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class DarbuListas(models.Model):
+    title = models.CharField("Pavadinimas", max_length=100, help_text="Pavadink šitą darbą kaip nors originaliai")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Darbu listas'
+        verbose_name_plural = 'Darbų listai'
+
+    def __str__(self):
+        return self.title
+        return self.user
+
+class DarboDuomenys(models.Model):
+    marke = models.CharField("A_marke", max_length=30, help_text="Automobilio markė")
+    modelis = models.CharField("A_modelis", max_length=20, help_text="Automobilio modelis")
+    sav = models.CharField("A_sav", max_length=70, help_text="Automobilio savininko vardas, pavardė")
+    suged = models.CharField("A_sugedimai", max_length=200, help_text="Automobilio sugedimai")
+    isCompleted = models.BooleanField()
+    list = models.ForeignKey(DarbuListas, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = 'Darbo itemas'
+        verbose_name_plural = 'Darbo itemai'
+
+    def __str__(self):
+        return self.text
---
+++ registracija/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from .models import DarbuListas
+
+# from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Create your views here.
+class VisuDarbuListai(ListView):
+    model = DarbuListas
+    template_name = 'home.html'
+
+    def get_queryset(self):
+        return DarbuListas.objects.filter(user=self.request.user)
+
+def list(request, id):
+    list = DarbuListas.objects.get(id=id)
+    if request.method == "POST":
+        print(request.POST)
+
+    if request.POST.get('save'):
+        checked = request.POST.getlist('checkbox')
+        for item in list.darboduomenys_set.all():
+            if str(item.id) in checked:
+                item.isCompleted = True
+            else:
+                item.isCompleted = False
+            item.save()
+    elif request.POST.get('add'):
+        text = request.POST.get('new')
+        if len(text) > 3:
+            list.darboduomenys_set.create(text=text, isCompleted=False)
+        else:
+            print("Užduoties tekstas per trumpas")
+    elif request.POST.get('delete'):
+        id_to_delete = request.POST.get('delete')
+        for item in list.darboduomenys_set.all():
+            if str(item.id) == id_to_delete:
+                item.delete()
+        return redirect('list', id=id)
+    else:
+        context = {"title":f"{list.title}","items": list.darboduomenys_set.all() }
+        return render(request, 'darbai.html', context=context)
