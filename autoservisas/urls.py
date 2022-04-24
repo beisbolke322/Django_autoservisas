912a63f782ee2e626ba33e8994b06a6ef0935fcb
beisbolke322 <mykolasp999@gmail.com>
2022-04-24 10:01:45 +0300

    dar tvarkyti

--- autoservisas/urls.py
+++ autoservisas/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('registracija.urls')),
     path('uzsakymai/', include('uzsakymai.urls')),
-    path('ppong/', ping_pong.as_view()),
+    path('ppong/', ),
 ]
