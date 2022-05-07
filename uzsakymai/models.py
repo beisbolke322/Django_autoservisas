38b74f6580fcc7261c2523b6f613868cebda14f1
beisbolke322 <mykolasp999@gmail.com>
2022-05-07 14:09:43 +0300

    nebaigta

--- uzsakymai/models.py
+++ uzsakymai/models.py
@@ -15,6 +15,9 @@ class UzsakymuListas(models.Model):
 class Uzsakymai(models.Model):
     dalis = models.CharField("dalis", max_length=30, help_text="Reikalinga dalis")
     uzsakyta = models.BooleanField(default=False)
+    is_kur = models.CharField("vieta", max_length=30, help_text="Iš kur užsakyti", default="SA")
+    kiekis = models.SmallIntegerField("kiekis", help_text="Koks dalių kiekis reikalingas", default=0)
+    data = models.DateField(auto_now=True)
     list = models.ForeignKey(UzsakymuListas, on_delete=models.CASCADE, blank=True, null=True)
     
     class Meta:
@@ -23,4 +26,6 @@ class Uzsakymai(models.Model):
 
     def __str__(self):
         return self.dalis
+        return self.is_kur
+        return self.kiekis
        
--- uzsakymai/views.py
+++ uzsakymai/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .models import Uzsakymai
+from .models import Uzsakymai, UzsakymuListas
 
-# from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
 class VisiUzsakymai(View):
     model = Uzsakymai
     template_name = 'uzsakymai.html'
@@ -14,20 +12,27 @@ class VisiUzsakymai(View):
         return render(request, self.template_name, {'x': x})
     
     def post(self, request, *args, **kwargs):
-        new = Uzsakymai(dalis=request.POST.get('new'))
-        new.save()
-        x = Uzsakymai.objects.all()
-        return render(request, self.template_name, {'x': x})
+        if request.POST.get('submit') == 'add':
+            new = Uzsakymai(dalis=request.POST.get('new'))
+            new.save()
+            x = Uzsakymai.objects.all()
+            return render(request, self.template_name, {'x': x})
+        elif request.POST.get('submit') == 'uzsak':
+            print(request.POST)
+
+            new_u = Uzsakymai(uzsakyta=request.POST.get('uzsak'))
+            checked = request.POST.getlist('checkbox')
+            for id in checked:
+                id = int(id)
+            list = Uzsakymai.objects.get(id=id)
+            print(Uzsakymai.objects.all(), "+++", id)
+            for item in list.objects.all():
+                if str(item.id) in checked:
+                    item.uzsakyta = True
+                else:
+                    item.uzsakyta = False
+                new_u.save()
+            x = Uzsakymai.objects.all()
+            print(x)
+            return render(request, self.template_name, {'x': x})
         
-def list(request, id):
-    list = Uzsakymai.objects.get(id=id)
-    new_u = Uzsakymai(uzsakyta=request.POST.get('uzsak'))
-    checked = request.POST.getlist('checkbox')
-    print(uzsakyta=request.POST.get('uzsak'))
-    for item in list.uzsakymai_set.all():
-        if str(item.id) in checked:
-            item.uzsakyta = True
-        else:
-            item.uzsakyta = False
-        new_u.save()
-       
